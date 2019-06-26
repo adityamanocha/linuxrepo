72cfca2aef199497a63319a1681f3005da02a389
Aditya M <aditya.manocha@gmail.com>
2019-06-25 23:55:01 -0400

    New development in java program

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
   public static void main(String[] args) {
      //Print a statement on the terminal window
-     System.out.println("Hello, World");
+     System.out.println("Hello, World from development");
   }
 }
